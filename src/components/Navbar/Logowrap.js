6fb855a10b9d1b52b3acb79c1f50fe71ee835219
jacksmedia <alex@j4cks.com>
2021-07-19 08:37:39 -0700

    :golf::sparkles: best ever, styledcomp navbar

---
+++ src/components/Navbar/Logowrap.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const Logowrap = () => {
+	return(
+		<div>
+			<StaticImage
+				className="tiny-icon"
+				layout="fixed"
+				formats={["AUTO", "WEBP", "AVIF"]}
+				src="./logo.png"
+				width={64}
+				height={64}
+				quality={95}
+				alt="Jacks Media can help you!"
+        />
+		</div>
+	)
+}
+
+export default Logowrap
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,21 @@
 import * as React from "react"
-import Footsie from "../components/footsie"
+import PropTypes from "prop-types"
+import Footsie from "./Footsie"
+import Navbar from "./Navbar/Navbar"
 import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { StaticImage } from "gatsby-plugin-image"
 
 const Layout = ({ location, title, children }) => {
-  const ethWallet = "0xfe130864ccFfA959C2cAd1A96bC5c625748130b5"
-  const btcWallet = "1G4zZpnha3RvdbMxujreWhChXaeTvo3E62"
   const rootPath = `${__PATH_PREFIX__}/` // boolean test: y/n Home icon aside app title
   const isRootPath = location.pathname === rootPath
   let headerLogo
   if (isRootPath) {
     headerLogo = (
-      <h4 className="cinzel">
+      <h3 className="cinzel">
         {title}
-      </h4>
+      </h3>
     )
   } else {
     headerLogo = (
@@ -31,16 +31,8 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div>
-      <header className="navver">
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
-        <div className="level-item logo">{headerLogo}</div>
-        <ul className="menu">
-          <li><Link to="/wallets" className="is-hoverable">Wallets</Link></li>
-          <li><Link to="/authentication" className="is-hoverable">Auth</Link></li>
-          <li><Link to="/browser" className="is-hoverable">Browser</Link></li>
-          <li><Link to="/exchanges" className="is-hoverable">Exchanges</Link></li>
-        </ul>
+      <header>
+        <Navbar />
       </header>
       <section className="mains">{children}</section>
       <footer>
