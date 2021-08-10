272bb6aba6a7813aafcd7a75158ab92b981eafd7
jacksmedia <alex@j4cks.com>
2021-08-10 14:25:09 -0700

    :art: adds intersection-observer to landing pg

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,11 @@
 import * as React from 'react'
-import PropTypes from 'prop-types'
-import Footblur from './Footblur'
-import Navbar from './Navbar/Navbar'
 import { Link } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { StaticImage } from 'gatsby-plugin-image'
+import Navbar from './Navbar/Navbar'
+import Footblur from './Footblur'
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/` // boolean test: y/n Home icon aside app title
-  const isRootPath = location.pathname === rootPath
-  let headerLogo
-  if (isRootPath) {
-    headerLogo = (
-      <h3 className="cinzel">
-        {title}
-      </h3>
-    )
-  } else {
-    headerLogo = (
-      <h5 className="cinzel chromatic">
-        <Link to="/">
-        <FontAwesomeIcon icon={faHome} />
-          <span>&nbsp;</span>
-        {title}
-        </Link>
-      </h5>
-    )
-  } // end boolean Home icon feature
-
   return (
     <div>
       <header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React, { useState, setState } from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 import TransitionLink from "gatsby-plugin-transition-link"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Imager from "../components/imager"
+import IO from "../components/io"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const Post = styled.li`
+  border-radius: 1vw;
+  padding: 2rem;
+  margin: 4.85rem 0.85rem 1.85rem 0.85rem;
+  transition: background-color 3.3s ease;
+  background-color: ${({ isVisible }) => (isVisible ? 'snow' : ' black')};
+`
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -31,35 +41,39 @@ const BlogIndex = ({ data, location }) => {
             const title = post.frontmatter.title || post.fields.slug
 
             return (
-              <li key={post.fields.slug} className="box">
-                <article
-                  className="post-list-item chromatic"
-                  itemScope
-                  itemType="http://schema.org/Article"
-                >
-                  <Imager
-                    imageInfo={{
-                      image: post.frontmatter.featuredimage,
-                      alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                    }}
-                   />
-                  <header>
-                    <h2 className="bold">
-                      <AniLink paintDrip to={post.fields.slug} itemProp="url">
-                        <span itemProp="headline">{title}</span>
-                      </AniLink>
-                    </h2>
-                  </header>
-                  <section>
-                    <p
-                      dangerouslySetInnerHTML={{
-                        __html: post.frontmatter.description || post.excerpt,
+              <IO rootMargin = '-50px'>
+              {({isVisible})=>(
+                <Post key={post.fields.slug} isVisible={isVisible}>
+                  <article
+                    className="post-list-item chromatic"
+                    itemScope
+                    itemType="http://schema.org/Article"
+                  >
+                    <Imager
+                      imageInfo={{
+                        image: post.frontmatter.featuredimage,
+                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
                       }}
-                      itemProp="description"
-                    />
-                  </section>
-                </article>
-              </li>
+                     />
+                    <header>
+                      <h2 className="bold">
+                        <AniLink paintDrip to={post.fields.slug} itemProp="url">
+                          <span itemProp="headline">{title}</span>
+                        </AniLink>
+                      </h2>
+                    </header>
+                    <section>
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html: post.frontmatter.description || post.excerpt,
+                        }}
+                        itemProp="description"
+                      />
+                    </section>
+                  </article>
+                </Post>
+              )}
+              </IO>
             )
           })}
         </ol>
